82da743b3863a7c18947f6825a710206d3e431c8
Janel Galisfrancisco <janelfrancisco2122@gmail.com>
2025-10-20 23:01:35 +0800

    Code for from student record is added

--- ReadFile_Lab/FrmStudentRecord.Designer.cs
+++ ReadFile_Lab/FrmStudentRecord.Designer.cs
@@ -32,59 +32,95 @@
             this.btnRegister = new System.Windows.Forms.Button();
             this.btnFind = new System.Windows.Forms.Button();
             this.btnUpload = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lvShowText
             // 
             this.lvShowText.HideSelection = false;
-            this.lvShowText.Location = new System.Drawing.Point(29, 28);
+            this.lvShowText.Location = new System.Drawing.Point(29, 56);
             this.lvShowText.Name = "lvShowText";
             this.lvShowText.Size = new System.Drawing.Size(510, 238);
             this.lvShowText.TabIndex = 2;
             this.lvShowText.UseCompatibleStateImageBehavior = false;
+            this.lvShowText.View = System.Windows.Forms.View.List;
             // 
             // btnRegister
             // 
-            this.btnRegister.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegister.Location = new System.Drawing.Point(29, 315);
+            this.btnRegister.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegister.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnRegister.Location = new System.Drawing.Point(29, 328);
             this.btnRegister.Name = "btnRegister";
             this.btnRegister.Size = new System.Drawing.Size(124, 34);
             this.btnRegister.TabIndex = 3;
             this.btnRegister.Text = "Register";
-            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
             // btnFind
             // 
-            this.btnFind.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFind.Location = new System.Drawing.Point(215, 315);
+            this.btnFind.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnFind.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFind.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnFind.Location = new System.Drawing.Point(215, 328);
             this.btnFind.Name = "btnFind";
             this.btnFind.Size = new System.Drawing.Size(124, 34);
             this.btnFind.TabIndex = 4;
             this.btnFind.Text = "Find";
-            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
             // 
             // btnUpload
             // 
-            this.btnUpload.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpload.Location = new System.Drawing.Point(415, 315);
+            this.btnUpload.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnUpload.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUpload.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnUpload.Location = new System.Drawing.Point(415, 328);
             this.btnUpload.Name = "btnUpload";
             this.btnUpload.Size = new System.Drawing.Size(124, 34);
             this.btnUpload.TabIndex = 5;
             this.btnUpload.Text = "Upload";
-            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGreen;
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "View Records";
             // 
             // FrmStudentRecord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ReadFile_Lab.Properties.Resources.Dark_Green_Modern_Gradient_Wave_Linktree_Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(569, 396);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnUpload);
             this.Controls.Add(this.btnFind);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.lvShowText);
             this.Name = "FrmStudentRecord";
             this.Text = "FrmStudentRecord";
+            this.Load += new System.EventHandler(this.FrmStudentRecord_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -94,5 +130,7 @@
         private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.Button btnFind;
         private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- ReadFile_Lab/FrmStudentRecord.cs
+++ ReadFile_Lab/FrmStudentRecord.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +18,56 @@ namespace ReadFile_Lab
         {
             InitializeComponent();
         }
+
+        // 3.1 The Register button in FrmStudent should go back to the FrmRegistration
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            FrmRegistration frmRegistration = new FrmRegistration();
+            frmRegistration.Show();
+            this.Hide();
+
+        }
+
+        // 3.2 The Find button will look for the text file in the specified directory
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            DisplayToList();
+        }
+
+        private void DisplayToList()
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string _getText = "";
+                while ((_getText = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(_getText);
+                    lvShowText.Items.Add(_getText);
+                }
+            }
+        }
+
+
+        private void FrmStudentRecord_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 3.3 while the Upload button will only display a message box “Successfully Uploaded!”
+        // and will clear the ListView’s existing content.
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully Uploaded!", "Upload", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lvShowText.Items.Clear();
+
+        }
     }
-}
+
+    }
+
