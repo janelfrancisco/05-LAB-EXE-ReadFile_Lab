6a94787cda453fca7ed9ebb5cd871ec7f2515e44
Janel Galisfrancisco <janelfrancisco2122@gmail.com>
2025-10-20 23:00:48 +0800

    Display to list method added

--- ReadFile_Lab/Form1.Designer.cs
+++ ReadFile_Lab/Form1.Designer.cs
@@ -31,41 +31,67 @@
             this.lvShowText = new System.Windows.Forms.ListView();
             this.btnOpen = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lvShowText
             // 
+            this.lvShowText.Font = new System.Drawing.Font("Ebrima", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvShowText.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.lvShowText.HideSelection = false;
             this.lvShowText.Location = new System.Drawing.Point(30, 27);
+            this.lvShowText.MultiSelect = false;
             this.lvShowText.Name = "lvShowText";
             this.lvShowText.Size = new System.Drawing.Size(510, 238);
             this.lvShowText.TabIndex = 1;
             this.lvShowText.UseCompatibleStateImageBehavior = false;
+            this.lvShowText.View = System.Windows.Forms.View.List;
             // 
             // btnOpen
             // 
-            this.btnOpen.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOpen.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.btnOpen.Location = new System.Drawing.Point(229, 311);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(107, 34);
             this.btnOpen.TabIndex = 2;
             this.btnOpen.Text = "Open";
-            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Ebrima", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.Location = new System.Drawing.Point(409, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "View Records";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // FrmOpenTextFile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ReadFile_Lab.Properties.Resources.Dark_Green_Modern_Gradient_Wave_Linktree_Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(569, 396);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.lvShowText);
             this.Name = "FrmOpenTextFile";
             this.Text = "FrmOpenTextFile";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +100,7 @@
         private System.Windows.Forms.ListView lvShowText;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ReadFile_Lab/Form1.cs
+++ ReadFile_Lab/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,39 @@ namespace ReadFile_Lab
         public FrmOpenTextFile()
         {
             InitializeComponent();
+        }
+
+        // 5. Click the Open button to generate a click event.
+        // 6. Create a method named DisplayToList and add the following code
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            DisplayToList();
+        }
+
+        private void DisplayToList()
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string _getText = "";
+                while ((_getText = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(_getText);
+                    lvShowText.Items.Add(_getText);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            FrmStudentRecord frmStudentRecord = new FrmStudentRecord();
+            frmStudentRecord.Show();
+            this.Hide();
         }
     }
 }
