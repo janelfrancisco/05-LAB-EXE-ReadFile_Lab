28939258e705f6d305cf4cde59241dfad803b826
Janel Galisfrancisco <janelfrancisco2122@gmail.com>
2025-10-20 23:02:40 +0800

    Form Registration UI copied and code modified, text file changed

--- ReadFile_Lab/FrmRegistration.Designer.cs
+++ ReadFile_Lab/FrmRegistration.Designer.cs
@@ -48,6 +48,7 @@
             this.txtBirthday = new System.Windows.Forms.DateTimePicker();
             this.txtStudentNo = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtGender
@@ -229,12 +230,13 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(331, 384);
+            this.button1.Location = new System.Drawing.Point(418, 367);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 40);
             this.button1.TabIndex = 24;
             this.button1.Text = "Register";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtBirthday
             // 
@@ -263,6 +265,20 @@
             this.label1.TabIndex = 21;
             this.label1.Text = "Registration";
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(235, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 40);
+            this.button2.TabIndex = 41;
+            this.button2.Text = "Records";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // FrmRegistration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -270,6 +286,7 @@
             this.BackgroundImage = global::ReadFile_Lab.Properties.Resources.Dark_Green_Modern_Gradient_Wave_Linktree_Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.txtGender);
             this.Controls.Add(this.txtprogram);
             this.Controls.Add(this.txtMiddleInitial);
@@ -319,5 +336,6 @@
         private System.Windows.Forms.DateTimePicker txtBirthday;
         private System.Windows.Forms.TextBox txtStudentNo;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ReadFile_Lab/FrmRegistration.cs
+++ ReadFile_Lab/FrmRegistration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,44 @@ namespace ReadFile_Lab
         public FrmRegistration()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string studentNo = txtStudentNo.Text;
+            string program = txtprogram.Text;
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            string middleInitial = txtMiddleInitial.Text;
+            string age = txtAge.Text;
+            string birthday = txtBirthday.Text;
+            string contactNo = txtContactNo.Text;
+            string gender = txtGender.Text;
+
+
+
+            string relativePath = @"..\..\Text Files_Galisfrancisco";
+            string docPath = Path.GetFullPath(relativePath);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, txtStudentNo.Text + ".txt")))
+            {
+                outputFile.WriteLine("Student Number: " + studentNo);
+
+                outputFile.WriteLine("Program: " + program);
+                outputFile.WriteLine("Full Name: " + firstName + " " + middleInitial + " " + lastName);
+                outputFile.WriteLine("Age: " + age);
+                outputFile.WriteLine("Birthday: " + birthday);
+                outputFile.WriteLine("Gender: " + gender);
+                outputFile.WriteLine("Contact No.: " + contactNo);
+            }
+
+            MessageBox.Show("Student Registered in" + docPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrmStudentRecord frmStudentRecord = new FrmStudentRecord();
+            frmStudentRecord.Show();
+            this.Hide();
         }
     }
 }
